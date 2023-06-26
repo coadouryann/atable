88995d6c9395ea97d1c3ac7dab82643381646a16
COADOUR Yann <105500388+coadouryann@users.noreply.github.com>
2023-06-26 21:05:21 +0200

    Premier commit

---
+++ src/main/java/com/yann/atable/AtableApplication.java
@@ -0,0 +1,13 @@
+package com.yann.atable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yann/atable/AtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yann.atable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
